47d8cbccd790c0416cc92029b319644fb2317fcb
hwan <luz@hwanui-MacBookPro.local>
2025-03-05 23:45:56 +0900

    commit

--- src/CommentJava.java
+++ src/CommentJava.java
@@ -1,5 +1,14 @@
-public class CommnetJava {
+public class CommentJava {
     public static void main(String[] args) {
-        
+        //한 줄 주석
+        System.out.println("한 줄 주석1");
+//        System.out.println("한 줄 주석2");
+        System.out.println("한 줄 주석3");
+//        System.out.println("한 줄 주석4");
+        System.out.println("한 줄 주석5");
+        /* 여러 줄 주석
+          System.out.println("여러 줄 주석");
+          System.out.println("모두 주석처리");
+        */
     }
 }
