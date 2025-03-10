4ee2d73ea730d573732ba80b4dc42d992ac957e5
luz <luz314314@gmail.com>
2025-03-10 20:10:22 +0900

    var

--- src/variable/Var2.java
+++ src/variable/Var2.java
@@ -10,11 +10,14 @@ public class Var2 {
         int c = 50;
         c = 40;
 
+        int d = -10;
+
         System.out.println(a);
         System.out.println(a);
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
+        System.out.println(d);
     }
 
 }
