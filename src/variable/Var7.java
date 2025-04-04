3e306d7bf9783d42db0d79a41513553e2475c3f4
luz <luz314314@gmail.com>
2025-04-04 23:37:32 +0900

    char

--- src/variable/Var7.java
+++ src/variable/Var7.java
@@ -3,6 +3,12 @@ package variable;
 public class Var7 {
     public static void main(String[] args) {
         char a = 'a';
+
+        char b;
+        b = 'b';
+
         System.out.println(a);
+        System.out.println(b);
+
     }
 }
