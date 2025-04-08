84b00409649d8dac980c5566ae03cf777b0da4b5
luz <luz314314@gmail.com>
2025-04-08 23:57:26 +0900

    boolean

--- src/variable/Var8.java
+++ src/variable/Var8.java
@@ -2,6 +2,11 @@ package variable;
 
 public class Var8 {
     public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
 
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a=b);
     }
 }
